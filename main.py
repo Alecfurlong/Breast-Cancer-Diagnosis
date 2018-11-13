f1527702213a69ee6a758db0bfbac571db033770
David Sun <whetfire@users.noreply.github.com>
2018-11-13 18:12:30 -0500

    upgraded to python3

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import processData
 
 X, F, id_dict = processData.clean("raw_breast_cancer_data.csv")
 
-print "id -list"
-print id_dict
-print 
+print("id -list")
+print(id_dict)
+print()
 
-print "Feature Definitions"
-print F
-print 
+print("Feature Definitions")
+print(F)
+print()
 
-print "Data"
-print X
+print("Data")
+print(X)
