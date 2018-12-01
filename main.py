5665068b2779af78b9f82eedc60f00aa10943d67
David Sun <whetfire@users.noreply.github.com>
2018-12-01 16:45:01 -0500

    improved roc graph

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # imported libraries
 import numpy as np
 import matplotlib.pyplot as pp
+import warnings
 
 # group project code
 import crossValidation as cv
@@ -13,15 +14,18 @@ X, y, F, id_list = processData.clean("raw_breast_cancer_data.csv")
 F = F[1:]
 print(F)
 
-# deleting highly correlated predictor variables where |correlation| > 0.9
-#F, X = diagnostics.deleteColumns([2, 3, 12, 13, 20, 21, 22, 23, 24], F, X)
+# delete highly correlated predictor variables where |correlation| > 0.9
+# F, X = diagnostics.deleteColumns([2, 3, 12, 13, 20, 21, 22, 23, 24], F, X)
 
 # counts of types of cancer
 print("malignant count: " + str(np.sum(y == 1)))
 print("benign count: " + str(np.sum(y == -1)))
-cv.nestedValidation(X, y, 'rbf')
 
-cv.nestedKFoldValidation(10, X, y, 'rbf')
+with warnings.catch_warnings():
+    cv.nestedKFoldValidation(10, X, y, 'linear')
+    # cv.nestedKFoldValidation(10, X, y, 'rbf')
+    # cv.nestedKFoldValidation(10, X, y, 'poly')
+    # cv.nestedValidation(X, y, 'rbf')
 
 # two fold cross validation, C=1.0, linear kernel
 # cv.linearTwoFold(X, y, 1.0, 'linear')
--- roc.py
+++ roc.py
@@ -2,56 +2,69 @@ import numpy as np
 import leaveOneOut as loocv
 import matplotlib.pyplot as pp
 from sklearn import metrics
+import processData
 
 def generateROC(X,y):
-	#get true positive rate, false positive rate
-	tpr, fpr = get_true_false_rates(X,y)
-	
-	pp.figure()
-	pp.xlabel("False Positive Rate")
-	pp.ylabel("True Positive Rate")
-	pp.title("ROC Curve")
+    #get true positive rate, false positive rate
+    tpr, fpr = get_true_false_rates(X,y)
+    
+    pp.figure()
+    pp.xlabel("False Positive Rate")
+    pp.ylabel("True Positive Rate")
+    pp.title("ROC Curve")
 
-	#calc area under curve and plot
-	area_under_curve = metrics.auc(fpr, tpr)
-	roc_label = '{} (AUC={:.3f})'.format('Method', area_under_curve)
-	pp.plot(fpr, tpr, color='orange', linewidth=2, label=roc_label)
+    #calc area under curve and plot
+    area_under_curve = metrics.auc(fpr, tpr)
+    roc_label = '{} (AUC={:.3f})'.format('Method', area_under_curve)
+    pp.plot(fpr, tpr, color='orange', linewidth=2, label=roc_label)
 
-	#plot linear line
-	x = [0.0, 1.0]
-	pp.plot(x, x, linestyle='dashed', color='blue', linewidth=2, label='random')
-	pp.xlim(0.0, 1.0)
-	pp.ylim(0.0, 1.0)
-	pp.legend(fontsize=10, loc='best')
-	pp.tight_layout()
+    #plot linear line
+    x = [0.0, 1.0]
+    pp.plot(x, x, linestyle='dashed', color='blue', linewidth=2, label='random')
+    pp.xlim(0.0, 1.0)
+    pp.ylim(0.0, 1.0)
+    pp.legend(fontsize=10, loc='best')
+    pp.tight_layout()
 
-	pass
+    pp.show()
 
 def get_true_false_rates(X,y):
-	tpr = [0.0]  # true positive rate
-	fpr = [0.0]  # false positive rate
-	
-	#real values
-	malignantCount = float(np.sum(y == 1))
-	benignCount = float(np.sum(y == -1))
+    tpr = [0.0]  # true positive rate
+    fpr = [0.0]  # false positive rate
+    
+    #real values
+    malignantCount = float(np.sum(y == 1))
+    benignCount = float(np.sum(y == -1))
 
-	correctlyClassifiedMal = 0.0
-	correctlyClassifiedBen = 0.0
+    truePositives = 0
+    falsePositives = 0
+    correctlyClassifiedMal = 0.0
+    correctlyClassifiedBen = 0.0
 
-	#get predictions using leave one out cross validation
-	y_pred = loocv.run(X,y,1.0,'linear')
+    #get predictions using leave one out cross validation
+    y_pred = loocv.run(X, y, 0.01, 'linear')
 
-	for i in range(len(X)):
-		#if y[i] matches prediction for X[i]
-		if y[i] == y_pred[i]:
-			if y[i] == 1:
-				#correctly classified Malignant
-				correctlyClassifiedMal += 1.0
-			else:
-				#correctly classified Malignant
-				correctlyClassifiedBen += 1.0
-		
-		tpr.append(correctlyClassifiedMal / malignantCount)
-		fpr.append(correctlyClassifiedBen / benignCount)
+    for i, ypredi in enumerate(y_pred):
+        # only worry about predicted positives
+        if ypredi == 1:
+            # true positive
+            if y[i] == 1:
+                truePositives += 1
+            # false positive
+            else:
+                falsePositives += 1
 
-	return tpr, fpr
+        tpr.append(truePositives / malignantCount)
+        fpr.append(falsePositives / benignCount)
+
+    tpr.append(1.0)
+    fpr.append(1.0)
+
+    return tpr, fpr
+
+def main():
+    X, y, F, ids = processData.clean('raw_breast_cancer_data.csv')
+    generateROC(X, y)
+
+if __name__ == '__main__':
+    main()
