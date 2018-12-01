2a9731c879585b4280d79f82c68474367b5ef935
Alec Furlong <alec@furlong.com>
2018-12-01 10:56:41 -0500

    4% Error RBF Kernel

--- main.py
+++ main.py
@@ -14,13 +14,14 @@ F = F[1:]
 print(F)
 
 # deleting highly correlated predictor variables where |correlation| > 0.9
-F, X = diagnostics.deleteColumns([2, 3, 12, 13, 20, 21, 22, 23, 24], F, X)
+#F, X = diagnostics.deleteColumns([2, 3, 12, 13, 20, 21, 22, 23, 24], F, X)
 
 # counts of types of cancer
 print("malignant count: " + str(np.sum(y == 1)))
 print("benign count: " + str(np.sum(y == -1)))
+cv.nestedValidation(X, y, 'rbf')
 
-cv.nestedKFoldValidation(10, X, y, 'linear')
+cv.nestedKFoldValidation(10, X, y, 'rbf')
 
 # two fold cross validation, C=1.0, linear kernel
 # cv.linearTwoFold(X, y, 1.0, 'linear')
